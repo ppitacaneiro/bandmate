d93f65780828d69a225881f7decd5923029ab00c
Pablo Pita <ppitacaneiro@gmail.com>
2024-02-13 11:14:36 +0100

    latest registers components

---
+++ src/data/fake.js
@@ -0,0 +1,65 @@
+export const musicians = [
+	{
+		location: 'New York, NY',
+        name: 'John Doe',
+        style: 'Rock',
+		avatar: 'https://www.w3schools.com/w3images/avatar2.png',
+	},
+	{
+		location: 'Los Angeles, CA',
+		name: 'Jane Doe',
+		style: 'Pop',
+		avatar: 'https://www.w3schools.com/w3images/avatar6.png',
+	},
+	{
+		location: 'Nashville, TN',
+		name: 'John Smith',
+		style: 'Country',
+		avatar: 'https://www.w3schools.com/w3images/avatar5.png',
+	},
+	{
+		location: 'Austin, TX',
+		name: 'Jane Smith',
+		style: 'Blues',
+		avatar: 'https://www.w3schools.com/w3images/avatar4.png',
+	},
+	{
+		location: 'Seattle, WA',
+		name: 'John Johnson',
+		style: 'Jazz',
+		avatar: 'https://www.w3schools.com/w3images/avatar3.png',
+	},
+];
+
+export const bands = [
+	{
+		location: 'New York, NY',
+		name: 'The Beatles',
+		style: 'Rock',
+		avatar: 'https://www.w3schools.com/w3images/avatar2.png',
+	},
+	{
+		location: 'Los Angeles, CA',
+		name: 'The Rolling Stones',
+		style: 'Pop',
+		avatar: 'https://www.w3schools.com/w3images/avatar6.png',
+	},
+	{
+		location: 'Nashville, TN',
+		name: 'The Eagles',
+		style: 'Country',
+		avatar: 'https://www.w3schools.com/w3images/avatar5.png',
+	},
+	{
+		location: 'Austin, TX',
+		name: 'The Beach Boys',
+		style: 'Pop',
+		avatar: 'https://www.w3schools.com/w3images/avatar4.png',
+	},
+	{
+		location: 'Seattle, WA',
+		name: 'The Doors',
+		style: 'Rock',
+		avatar: 'https://www.w3schools.com/w3images/avatar3.png',
+	}	
+];
